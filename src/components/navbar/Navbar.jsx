69e5cd322b9f8ee751c3bcd88b27e3f672a38da1
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-09-11 13:38:03 -0400

    Responsive menu (burger menu)

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,9 +17,15 @@ const Navbar = () => {
       <div className={styles.logo}>NomadDev</div>
       <div className={styles.links}>
         <ThemeToggle />
-        <Link href="/">Homepage</Link>
-        <Link href="/">Contact</Link>
-        <Link href="/">About</Link>
+        <Link className={styles.link} href="/">
+          Homepage
+        </Link>
+        <Link className={styles.link} href="/">
+          Contact
+        </Link>
+        <Link className={styles.link} href="/">
+          About
+        </Link>
         <AuthLinks />
       </div>
     </div>
