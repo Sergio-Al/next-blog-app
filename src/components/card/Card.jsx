dfb0a0032f04819baf3ab0b54451574faafe0d28
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-09-12 10:27:57 -0400

    Post and side menu design

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import styles from "./card.module.css";
+import Link from "next/link";
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>11.02.2023 - </span>
+          <span className={styles.category}>CULTURE</span>
+        </div>
+        <Link href="/">
+          <h1>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur,
+            laborum.
+          </h1>
+        </Link>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+          saepe, aperiam dolorum odio omnis vero labore. Rerum veniam earum
+          possimus pariatur? Voluptatem corporis iusto doloribus repellat,
+          pariatur rem quia sequi.
+        </p>
+        <Link href="/" className={styles.link}>
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
