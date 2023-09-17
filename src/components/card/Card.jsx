900710aa4029ebe1475c2566d576ab684bf9b0ec
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-09-17 11:54:15 -0400

    Send query to api (fetch post by category)

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,24 +5,23 @@ import Link from "next/link";
 const Card = ({ key, item }) => {
   return (
     <div className={styles.container} key={key}>
-      <div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-      </div>
+      {item.image && (
+        <div className={styles.imageContainer}>
+          <Image src={item.image} alt="" fill className={styles.image} />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.02.2023 - </span>
-          <span className={styles.category}>CULTURE</span>
+          <span className={styles.date}>
+            {item.createdAt.substring(0, 10)} -{" "}
+          </span>
+          <span className={styles.category}>{item.catSlug}</span>
         </div>
-        <Link href="/">
+        <Link href={`/post/${item.slug}`}>
           <h1>{item.title}</h1>
         </Link>
-        <p className={styles.desc}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-          saepe, aperiam dolorum odio omnis vero labore. Rerum veniam earum
-          possimus pariatur? Voluptatem corporis iusto doloribus repellat,
-          pariatur rem quia sequi.
-        </p>
-        <Link href="/" className={styles.link}>
+        <p className={styles.desc}>{item.desc.substring(0, 60)}</p>
+        <Link href={`/post/${item.slug}`} className={styles.link}>
           Read More
         </Link>
       </div>
